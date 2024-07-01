8f259e96e0197ff2b299985a4e9039d5f51a97f9
lahaina <nattolecats@gmail.com>
2024-07-02 01:13:31 +0900

    penang: nfc: Pull libnfc-nxp.conf

--- device.mk
+++ device.mk
@@ -121,6 +121,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-mtp-SN100.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-mtp-SN100.conf
 
 # Perf
